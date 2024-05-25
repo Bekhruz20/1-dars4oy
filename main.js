8a7988afedcba5fc911bf3f14035f117e82ceebe
Bekhruz20 <163981178+Bekhruz20@users.noreply.github.com>
2024-05-25 10:45:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let youname=prompt('Ismingizni kiriting:')
+let surname=prompt('Familiyangizni kiriting:')
+let age=prompt('Yoshingizni kiriting:')
+let position=prompt('Turar joyingizni kiriting:')
+let hobbi=prompt('Yoqtirgan faningizni kiriting:')
+
+let str="Javoblar uchun rahmat"
+
+alert(str)
+
+console.log(youname)
+console.log(surname)
+console.log(age)
+console.log(position)
+console.log(hobbi)
